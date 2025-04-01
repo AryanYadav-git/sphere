2c8dc7a9ef2de5b0e7042564dd543f26bc8868d8
Aryan Yadav <yadavaryan466@gmail.com>
2025-04-01 19:32:10 +0530

    added create sprint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [user, setUser] = useState();
   const handleSubmit = async () => {
     const project = {
       name: 'New Project',
@@ -56,11 +57,57 @@ const App = () => {
       console.error('Error:', error)
     }
   }
+  const handleAddNewSprint = async () => {
+    const member = {
+      projectId: 14,
+      name: 'User Authentication',
+      goal: 'Implement User Authentication System',
+      startDate: '2015-10-12',
+      endDate: '2025-10-12',
+    }
+
+    try {
+      const response = await fetch('/api/project/create-new-sprint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(member),
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to create project')
+      }
+
+      const data = await response.json()
+      console.log('sprint created:', data)
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('/api/me')
+        const data = await response.json();
+
+        console.log('User data:', data.user.email)
+        setUser(data.user.email);
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    }
+
+    fetchUserData();
+  },[])
 
   return (
-    <div>
-      <button onClick={handleSubmit}>Create Project</button>
-      <button onClick={handleAddNewMember}>Add new memeber</button>
+    <div className='flex flex-col gap-6 items-center w-screen'>
+      <p>{user}</p>
+      <button onClick={handleSubmit} className='w-fit'>Create Project</button>
+      <button onClick={handleAddNewMember} className='w-fit'>Add new memeber</button>
+      <button onClick={handleAddNewSprint} className='w-fit'>Add new sprint</button>
     </div>
   )
 }
